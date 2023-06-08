4de35daf88b0ab82c6c4b25cf3d63350dba24faf
Prashant Singh Ranawat <90084352+ranaji038@users.noreply.github.com>
2023-06-08 12:57:38 +0530

    Time: 25 ms (37.93%), Space: 19.5 MB (96.07%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -3,20 +3,34 @@ public:
     int majorityElement(vector<int>& nums) {
         
                 int n = nums.size();
-        	unordered_map<int,int> mpp;
+//         	unordered_map<int,int> mpp;
 
-	for(int i = 0 ; i < n ; i++){
-		mpp[nums[i]]++;
+// 	for(int i = 0 ; i < n ; i++){
+// 		mpp[nums[i]]++;
 
-	}
+// 	}
 
 
-	for(auto it : mpp){
+// 	for(auto it : mpp){
 
-		if(it.second > n/2) return it.first;
-	}
+// 		if(it.second > n/2) return it.first;
+// 	}
 
-        return -1;    
+//         return -1;    
+            
+            // Moores Voting ALgorithm
+            int cnt = 0 ; int el;
+           
+            for(int i = 0 ; i < n ; i++){
+                    if(cnt == 0){
+                            el = nums[i];
+                            cnt = 1;
+                            
+                    }else if(nums[i] == el) cnt++;
+                    else cnt--;
+            }
+            
+            return el;
             
     }
 };

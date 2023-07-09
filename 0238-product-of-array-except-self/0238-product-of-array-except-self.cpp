38738d0d43d243ef900d3e82bf4b6922d8f4aff9
Prashant Singh Ranawat <90084352+ranaji038@users.noreply.github.com>
2023-07-09 18:25:27 +0530

    Time: 143 ms (6.32%), Space: 24.4 MB (51.90%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -7,24 +7,58 @@ public:
     vector<int> productExceptSelf(vector<int>& nums) {
                 int n = nums.size();
             
-            vector<int> left(n,1);
-            vector<int> right(n,1);
-            vector<int> ans(n,1);
-            left[0] = 1;
-            for(int i = 1 ; i < n ;i++){
-                    left[i] = left[i-1]*nums[i-1];
-            }
-            print(left);
-            right[n-1] = 1;
+            // O(1) Space ans
             
-            for(int j = n-2 ; j>=0 ;j--){
-                    right[j] = right[j+1]*nums[j+1];
+            vector<int> ans(n,1);
+            int lp = 1;
+            for(int i = 0 ; i < n ;i++){
+                    ans[i] = lp;
+                    lp *= nums[i];
             }
-            print(right);
-            for(int i =0 ;i < n ;i++)
-        ans[i] = left[i]*right[i];
+            // print(ans);
+           
+            int rp =1;
+            for(int j = n-1 ; j>=0 ;j--){
+                    ans[j] *= rp;
+                    
+                    rp *= nums[j];
+            }
+            print(ans);
+            // for(int i =0 ;i < n ;i++)
+        // ans[i] = left[i]*right[i];
             
             return ans;
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            // BIG O(2n) Space
+            
+//             vector<int> left(n,1);
+//             vector<int> right(n,1);
+//             vector<int> ans(n,1);
+//             left[0] = 1;
+//             for(int i = 1 ; i < n ;i++){
+//                     left[i] = left[i-1]*nums[i-1];
+//             }
+//             // print(left);
+//             right[n-1] = 1;
+            
+//             for(int j = n-2 ; j>=0 ;j--){
+//                     right[j] = right[j+1]*nums[j+1];
+//             }
+//             // print(right);
+//             for(int i =0 ;i < n ;i++)
+//         ans[i] = left[i]*right[i];
+            
+//             return ans;
             
             
             

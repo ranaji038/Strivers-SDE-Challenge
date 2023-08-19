4d87206cfe938acee74244f478f7ca8ced74fce9
Prashant Singh Ranawat <90084352+ranaji038@users.noreply.github.com>
2023-08-19 16:42:25 +0530

    Added solution - LeetHub

--- Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
+++ Minimum Spanning Tree - GFG/minimum-spanning-tree.cpp
@@ -3,6 +3,76 @@
 using namespace std;
 
 // } Driver Code Ends
+
+
+class disjointSet{
+vector<int> rank , parent,size;
+public :
+    
+    
+    disjointSet(int x){
+        rank.resize(x + 1);
+        parent.resize(x + 1);
+        size.resize(x+1);
+        for(int i =0 ; i< x ;i++){
+            parent[i] =  i;
+            rank[i] = 0;
+            size[i] = 1;
+        }
+        
+    }
+    int findUpar(int node){
+        if(node == parent[node]) return node;
+        
+        return parent[node] = findUpar(parent[node]);
+    }
+    
+    void unionbyrank(int x , int y){
+        int parent_x = findUpar(x);
+        int parent_y = findUpar(y);
+        
+        if(parent_x == parent_y) return ;
+      
+            if(rank[parent_x] < rank[parent_y]){
+                
+                parent[parent_x] = parent_y;
+                
+            }else if(rank[parent_x] > rank[parent_y]){
+                 parent[parent_y] = parent_x;
+            }else{
+                   parent[parent_y] = parent_x;
+                   rank[parent_x]++;
+            }
+        
+        
+        
+        
+     }
+      void unionbysize(int x , int y){
+        int parent_x = findUpar(x);
+        int parent_y = findUpar(y);
+        
+        if(parent_x == parent_y) return ;
+      
+            if(size[parent_x] < size[parent_y]){
+                
+                parent[parent_x] = parent_y;
+                size[parent_y] += size[parent_x];
+                
+            }else{
+                   parent[parent_y] = parent_x;
+                   size[parent_x] += size[parent_y];
+            }
+        
+        
+        
+        
+     }
+    
+    
+    
+    
+};
 class Solution
 {
 	public:
@@ -10,33 +80,35 @@ class Solution
     int spanningTree(int V, vector<vector<int>> adj[])
     {
         // code here
-        priority_queue<pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        
-        vector<int> vis(V,0);
-        pq.push({0,0}); // {wt,parent}
-        int sum =0;
-        while(!pq.empty()){
-            auto it = pq.top();
-            int node = it.second;
-            int wt = it.first;
-            pq.pop();
+        vector<pair<int,pair<int,int>>> edges;
+        for(int i = 0 ; i < V ;i++)
+        for(auto it : adj[i]){
+            int adjNode = it[0];
+            int wt = it[1];
+            int node = i;
+            edges.push_back({wt,{node,adjNode}});
             
-            if(vis[node] == 1) continue;
-            //add to mdt;
-            vis[node] = 1;
-            sum += wt;
-            
-            for(auto it : adj[node]){
-                int adjNode = it[0];
-                int wt = it[1];
-                if(!vis[adjNode]){
-                    pq.push({wt,adjNode});
-                }
-            }
-            
-             
         }
-        return sum;
+        
+       disjointSet ds(V);
+       
+       sort(edges.begin() , edges.end());
+       
+       int mst = 0;
+       
+       for(auto it : edges){
+           int wt = it.first;
+           int u = it.second.first;
+           int v = it.second.second;
+           
+           if(ds.findUpar(u) != ds.findUpar(v)){
+               mst += wt;
+               ds.unionbysize(u,v);
+           }
+       }
+       
+       return mst;
+        
         
     }
 };

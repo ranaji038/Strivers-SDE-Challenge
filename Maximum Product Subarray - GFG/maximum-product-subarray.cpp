80072f56af74669ff89e3392755a95b414b51ce7
Prashant Singh Ranawat <90084352+ranaji038@users.noreply.github.com>
2023-08-28 01:47:40 +0530

    Added solution - LeetHub

---
+++ Maximum Product Subarray - GFG/maximum-product-subarray.cpp
@@ -0,0 +1,45 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:
+
+	// Function to find maximum product subarray
+	long long maxProduct(vector<int> arr, int n) {
+	    // code here
+	    long long pref = 1 , suff =1 ;
+	    long long maxi = INT_MIN;
+	    for(int i  = 0 ; i < n ; i++){
+	        if(pref== 0) pref = 1;
+	        if(suff == 0) suff = 1;
+	        pref = pref*arr[i];
+	        suff = suff*arr[n-1 -i];
+	        maxi = max(maxi , max(pref , suff));
+	    }
+	    return maxi;
+	}
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, i;
+        cin >> n;
+        vector<int> arr(n);
+        for (i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        auto ans = ob.maxProduct(arr, n);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

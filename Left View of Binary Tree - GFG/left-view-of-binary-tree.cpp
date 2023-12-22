fb512619dd92be6446631f95887f11563d0ee568
Prashant Singh Ranawat <90084352+ranaji038@users.noreply.github.com>
2023-12-22 11:45:51 +0530

    Added solution - LeetHub

--- Left View of Binary Tree - GFG/left-view-of-binary-tree.cpp
+++ Left View of Binary Tree - GFG/left-view-of-binary-tree.cpp
@@ -126,45 +126,34 @@ struct Node
     }
 };
  */
+ 
 
-void solve(Node * root , int level , vector<int> &ans){
-    if(root == NULL) return;
-    
-    if(level == ans.size()) ans.push_back(root->data);
-    
-    solve(root->left, level +1 ,ans);
-    solve(root->right ,level +1 , ans);
-}
 
 //Function to return a list containing elements of left view of the binary tree.
 vector<int> leftView(Node *root)
 {
    // Your code here
-   vector<int> ans;
-    // if(root == NULL )return ans;
+   if(root == NULL) return {};
     
-    // queue<pair<Node * , int>> q;
-    // q.push({root,0});
-    // map<int,int> mpp;
+    vector<int> ans;
+    queue<Node * > q;
+    q.push(root);
     
-    // while(!q.empty()){
-    //     Node * curr = q.front().first;
-    //     int level = q.front().second;\
-    //     q.pop();
+    while(!q.empty()){
+        Node * top = q.front();
         
-    //     if(mpp.find(level) == mpp.end()){
-    //         mpp[level] = curr->data;
-    //     }
+        int n = q.size();
+        for(int i = 0 ; i < n ; i++){
+            Node *temp = q.front();
+            if(temp->left != NULL) q.push(temp->left);
+            if(temp->right != NULL) q.push(temp->right);
+            q.pop();
+        }
         
-    //     if(curr->left) q.push({curr->left,level+1});
+       ans.push_back(top->data);
         
-    //     if(curr->right) q.push({curr->right,level+1});
-        
-        
-    // }
-    // for(auto it : mpp)ans.push_back(it.second);
+    }
     
-    solve(root,0,ans);
-    
-   return ans;
+    return ans;
+   
 }
